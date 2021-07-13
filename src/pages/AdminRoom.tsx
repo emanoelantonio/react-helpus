36b757c90279eb65967d5c60a537d5377e651bff
emanoelantonio <dev.emanoel@protonmail.com>
2021-07-12 21:42:30 -0300

    Destaque e respondida pergunta

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -4,6 +4,8 @@ import { RoomCode } from '../components/RoomCode';
 
 import logoImg from '../assets/images/logo.svg';
 import deleteImg from '../assets/images/delete.svg';
+import checkImg from '../assets/images/check.svg';
+import answerImg from '../assets/images/answer.svg';
 import '../styles/room.scss';
 // import { useAuth } from '../hooks/useAuth';
 
@@ -37,6 +39,18 @@ export function AdminRoom() {
     }
   }
 
+  async function handleCheckQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+  }
+  
+  async function handleHighLightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true,
+    });
+  }
+
   return (
     <div id="page-room">
       <header>
@@ -62,7 +76,25 @@ export function AdminRoom() {
               key={question.id}
               content={question.content}
               author={question.author}
+              isAnswered={question.isAnswered}
+              isHighlighted={question.isHighlighted}
             >
+              {!question.isAnswered && (
+                <>
+                  <button
+                    type="button"
+                    onClick={()=> handleCheckQuestion(question.id)}
+                  >
+                  <img src={checkImg} alt="Marcar pergunta como respondida" />
+                  </button>
+                  <button
+                    type="button"
+                    onClick={()=> handleHighLightQuestion(question.id)}
+                  >
+                  <img src={answerImg} alt="Dar destaque à pergunta" />
+                  </button>
+                </>
+              )}
               <button
                 type="button"
                 onClick={()=> handleDeleteQuestion(question.id)}
